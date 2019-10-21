db5fdae06f0b644b3b62df8121d6acfc8d3032d4
Sinisa Mikulic <sinisa.mikulic@gmail.com>
2019-10-21 20:19:28 +0200

    Updates

---
+++ precache-manifest.f7232830c18fbade5f52a007c8f86cf3.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7ec0d6195e92e8a62ce6503cc269450a",
+    "url": "/trivia-app/index.html"
+  },
+  {
+    "revision": "703a5c76a6a55ef3dc2e",
+    "url": "/trivia-app/static/css/main.913a51a3.chunk.css"
+  },
+  {
+    "revision": "c24e09d1cc025ad80891",
+    "url": "/trivia-app/static/js/2.0be81c8d.chunk.js"
+  },
+  {
+    "revision": "703a5c76a6a55ef3dc2e",
+    "url": "/trivia-app/static/js/main.1f22fe1b.chunk.js"
+  },
+  {
+    "revision": "69940acafc3b5d9e6160",
+    "url": "/trivia-app/static/js/runtime-main.aba6ae97.js"
+  }
+]);
